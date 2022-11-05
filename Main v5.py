965bc6e61fdf9770e176c876da80572f37ed680e
Yashraj Mohanty <75062038+YashrajMohanty@users.noreply.github.com>
2022-11-05 20:01:48 +0530

    Update Main v5.py

--- Main v5.py
+++ Main v5.py
@@ -2,16 +2,20 @@ import cv2
 import numpy as np
 import torch
 
-#os.system('C:/Users/yashr/Desktop/CVproject/YOLOv7/detect.py --weights C:/Users/yashr/Desktop/CVproject/YOLOv7/yolov7.pt --source 0 --device 0 --img-size 768 --no-trace --exist-ok')
-#model = attempt_load('YOLOv7/yolov7.pt', map_location='cuda:0')  # load FP32 model
-
 model = torch.hub.load("YOLOv5", 'custom', path="YOLOv5/yolov5s.pt", source='local')
 
 capture = cv2.VideoCapture(0)
 while capture.isOpened():
     ret, frame = capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    #--- YOLOv5 ---
     results = model(frame)
+    #--- YOLOv5 ---
     cv2.imshow('YOLOv5', np.squeeze(results.render()))
+    cv2.imshow('Thresh', thresh)
     
     if cv2.waitKey(10) & 0xFF == ord('q'): #press q to quit
         break
