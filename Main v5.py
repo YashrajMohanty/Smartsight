71a2a94ed88c225063640cfc32f30d3254892961
Yashraj Mohanty <75062038+YashrajMohanty@users.noreply.github.com>
2023-04-02 20:50:13 +0530

    Revert "Delete Main v5.py"
    This reverts commit 285c9141f54180113128d26a96477f32954d52f4.

---
+++ Main v5.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import torch
+
+#os.system('python C:/Users/yashr/Desktop/GIT/Smartsight/YOLOv5/detect.py --weights C:/Users/yashr/Desktop/GIT/Smartsight/YOLOv5/yolov5s.pt --source C:/Users/yashr/Desktop/GIT/Smartsight/"Test videos"/"LA Walk "Outdoor.mp4')
+
+model = torch.hub.load("YOLOv5", 'custom', path="YOLOv5/yolov5s.pt", source='local')
+
+capture = cv2.VideoCapture('Test videos/LA Walk Park.mp4')
+while capture.isOpened():
+    ret, frame = capture.read()
+
+    #--- YOLOv5 ---
+    results = model(frame)
+    results_pd = results.pandas().xyxy[0]
+    try: #error handling in case number of records is zero
+        #print(results_pd['name'][0])
+        print(results_pd)
+    except:
+        pass
+    #--- YOLOv5 ---
+    
+    cv2.imshow('YOLOv5', np.squeeze(results.render()))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'): #press q to quit
+        break
+
+capture.release()
+cv2.destroyAllWindows()
