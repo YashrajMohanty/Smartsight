4ca217e93603c83480e0e16c1d7bbe7816046dc3
Yashraj Mohanty <75062038+YashrajMohanty@users.noreply.github.com>
2023-12-28 01:57:18 +0530

    Obj_detect_v8 switched to tensors. Depth_DNN changes
    Obj_detect_v8 now uses cuda tensor operations instead of ndarrays.
    Removed unnecessary operations and imports from Depth_DNN.
    Removed unused functions from custom_transforms

--- Depth_DNN.py
+++ Depth_DNN.py
@@ -1,11 +1,6 @@
-import numpy as np
-import cv2
-from time import time
 import torch
-from math import e
 from torchvision.transforms import v2
 from custom_transforms import small_transform
-
 
 class distance_estimation():
 
@@ -13,61 +8,51 @@ class distance_estimation():
         self.obstruction_flag = False
         self.caution_flag = False
 
-    def find_distance(self,disp, bb_center, display_text=True, fontscale=1):
-        if bb_center == None:
-            return
-        if len(bb_center) == 0:
-            return
-        distances = []
-
-        for i in bb_center:
-            x, y = i
-            x, y = int(x), int(y)
-
-            distance = disp[y,x]
-            distance = 1.42 + 40.08 * e**(-5.43 * distance)
-            distance = round(distance, 1)
-            distances.append(distance)
-
-            if display_text == True:
-                    cv2.putText(disp, str(distance)+'m', (x,y), cv2.FONT_HERSHEY_SIMPLEX, fontscale, (0,255,0), 1, cv2.LINE_AA)
-        return distances
 
     def place_markers(self,frame):
+        '''Measures values at certain points across the image
 
-        y_center, x_center = frame.shape[0]/2 , frame.shape[1]/2
+        Args:
+            frame (tensor): image frame
+
+        Returns:
+            None
+        '''
+
+        y_center, x_center = int(frame.shape[0]/2) , int(frame.shape[1]/2)
         grid_size = 160
         grid_x = [-grid_size, int(-grid_size/2), 0, int(grid_size/2), grid_size]
         grid_y = [int(-grid_size/2), 0, int(grid_size/2)]
-        #grid = [0]
 
-        marker_pos = []
+        dist = []
         
         for i in grid_x:
             for j in grid_y:
-                point = (x_center+i, y_center+j)
-                marker_pos.append(point)
-
-        dist = self.find_distance(frame, marker_pos, display_text=True, fontscale=0.5)
+                dist.append(round(frame[y_center+j, x_center+i].item(), 2))
+        
+        #print(dist)
+        #cv2.putText(frame, str(dist)+'m', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
 
         near_count = 0
         far_count = 0
         for i in dist:
-            if i < 3: # if distance returned by marker < 3 meters
+            if i > 0.75:
                 near_count += 1
-            if i > 15: # if distance returned by marker > 15 meters
+            if i < 0.2:
                 far_count += 1
 
         if near_count >= 4: # if 4 or more markers return low distance
             self.obstruction_flag = True
         else:
             self.obstruction_flag = False
-
+            
         if far_count == 15:
             self.caution_flag = True
         else:
             self.caution_flag = False
         return
+    
+
 
 
 class midas():
@@ -95,34 +80,33 @@ class midas():
 
 
     def predict_depth(self,frame):
+        '''Predict image depth using MiDaS
 
-        input_batch = small_transform(frame)
-        print(input_batch.shape)
+        Args:
+            frame (ndarray): image frame
+
+        Returns:
+            output (tensor): Depth map
+        '''
 
         with torch.no_grad():
-            prediction = self.midas(input_batch)
-            prediction = torch.nn.functional.interpolate(
-                prediction.unsqueeze(0), #.unsqueeze()
-                size=[256,256],#frame.shape[:2],
-                mode="bicubic",
-                align_corners=False).squeeze(1)
+            frame = v2.ToImageTensor()(frame)
+            frame = frame.to(midas.device) #[channel, height, width]
 
-        print(prediction.dtype, prediction.shape)
-        output = v2.Resize(size=(self.height, self.width), interpolation=v2.InterpolationMode.BICUBIC, antialias=False)(prediction)
-        output = torch.div(output, 1000) # map range from 0 to 1
-        #output = cv2.resize(output, (self.width, self.height), interpolation=cv2.INTER_NEAREST) # half img size
+            input_batch = small_transform(frame)
+
+            prediction = self.midas(input_batch) #[1, 256, 256]
+
+            output = torch.div(prediction, 1000) # map range from 0 to 1
+            output = v2.Resize(size=(self.height, self.width), interpolation=v2.InterpolationMode.BICUBIC, antialias=False)(output)
 
         return output
     
-
-    def convert_to_thermal(self, disp_map):
-        thermal = disp_map * 255
-        thermal = thermal.astype(np.uint8)
-        thermal = cv2.applyColorMap(thermal, cv2.COLORMAP_MAGMA)
-        return thermal
-    
     
 if __name__ == "__main__":
+    
+    import cv2
+    from time import time
 
     midas = midas()
     sd = distance_estimation()
@@ -131,7 +115,8 @@ if __name__ == "__main__":
     new_frame_time = 0
 
     print('Engaging test')
-    capture = cv2.VideoCapture('Chessboard/Stereo L anim.mp4')
+    capture = cv2.VideoCapture('Chessboard/LA Walk Park.mp4')
+
 
     while capture.isOpened():
 
@@ -140,30 +125,24 @@ if __name__ == "__main__":
         if (str(type(frame))) == "<class 'NoneType'>":
             print('Stream ended')
             break
-
-        frame = torch.from_numpy(frame)
-        frame = frame.to(midas.device)
-        frame = frame.permute([2,0,1])
-        #print(frame.shape) #[channel, height, width]
-
+        
         disp_map = midas.predict_depth(frame) # returns single channel image
         disp_map = disp_map.squeeze() # remove batch values of tensor
-        disp_map = disp_map.cpu().numpy()
+        
 
-        frame = frame.permute([1,2,0])
-        frame = frame.cpu().numpy()
-        #sd.place_markers(disp_map)
+        sd.place_markers(disp_map)
 
         new_frame_time = time()
         fps = int(1 / (new_frame_time - prev_frame_time))
         prev_frame_time = new_frame_time
+
+
+        disp_map = disp_map.cpu().numpy()
+
         cv2.putText(disp_map, str(fps), (0, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 1, cv2.LINE_AA)
 
         #cv2.imshow('frame',frame)
         cv2.imshow('Disp',disp_map)
-
-        
-        #cv2.imshow("Stereo", midas.convert_to_thermal(disp_map))
         
         #sleep(2)
         if cv2.waitKey(10) & 0xFF == ord('q'): #press q to quit
--- Obj_detect_v8.py
+++ Obj_detect_v8.py
@@ -1,6 +1,5 @@
-import cv2
 from ultralytics import YOLO
-import numpy as np
+import torch
 
 class obj_detect():
 
@@ -18,54 +17,53 @@ class obj_detect():
         boxes = self.boxes
         if len(boxes) == 0:
             return
+        
+        x_avg = torch.add(boxes[:,0], boxes[:,2])
+        y_avg = torch.add(boxes[:,1], boxes[:,3])
 
-        bb_center = []
+        bb_center = torch.stack((x_avg, y_avg), dim=1)
+        bb_center = torch.div(bb_center, 2, rounding_mode="trunc")
 
-        for box in boxes:
-            avg_x = int((box[0] + box[2])/2)
-            avg_y = int((box[1] + box[3])/2)
-            bb_center.append([avg_x, avg_y])
-            cv2.circle(frame, (avg_x, avg_y), 2, (0, 255, 0), -1)
         return bb_center
 
 
-    def filter_classes(self, result):
-        filter_class = [0,1,2,3,5,7,13,15,16,17,18,19,57] # classes to filter (according to COCO128)
-        result_boxes = result.boxes.xyxy.cpu().numpy()
-        result_cls = result.boxes.cls.cpu().numpy()     
-        boxes = []
-        cls = []
-        flag = False
-        for i in range(len(result_cls)):
+    def __filter_classes(self, result):
+        filter_class = [0,1,2,3,5,7,13,15,16,17,18,19,57] # classes to filter in (according to COCO128)
+        cls = result.boxes.cls.int() #[x1, y1, x2, y2]
+        boxes = result.boxes.xyxy.int()
+
+        new_cls = torch.tensor([], dtype=torch.int32, device=torch.device('cuda'))
+        new_boxes = torch.tensor([], dtype=torch.int32, device=torch.device('cuda'))
+
+        for i in range(len(cls)):
             for j in filter_class:
-                if result_cls[i] == j:
-                    flag = True
-                    continue
-            if flag:
-                cls.append(result_cls[i])
-                boxes.append(result_boxes[i])
-        cls = np.array(cls)
-        boxes = np.array(boxes)
-        return (boxes, cls)
+                if cls[i] == j:
+                    new_cls = torch.cat((new_cls, cls[i].unsqueeze(-1)),dim=0)
+                    new_boxes = torch.cat((new_boxes, boxes[i,:].unsqueeze(0)), dim=0)
+                    break
+
+        return (new_boxes, new_cls)
 
 
     def detect_objects(self, frame, filter_class=False):
         results = self.model.predict(source=frame, verbose=False)
-        if len(results) > 0:
+        if len(results):
             results_plot = results[0].plot(show_conf=False, line_width=1)
             for result in results:
                 if filter_class: # if using default model, filter classes
-                    self.boxes, self.cls = self.filter_classes(result)
+                    self.boxes, self.cls = self.__filter_classes(result)
                 else:
-                    self.boxes = result.boxes.xyxy.cpu().numpy()
-                    self.cls = result.boxes.cls.cpu().numpy()
+                    self.boxes = result.boxes.xyxy
+                    self.cls = result.boxes.cls
         return results_plot
 
 
 if __name__ == "__main__":
 
+    import cv2
+
     obj_det = obj_detect("ultralytics/yolov8n.pt")
-    capture = cv2.VideoCapture('Test videos/LA Walk Park.mp4')
+    capture = cv2.VideoCapture('Chessboard/LA Walk Park.mp4')
     #capture = cv2.VideoCapture('Chessboard/Stereo L anim.mp4')
     print('Engaging test')
     while capture.isOpened():
@@ -77,6 +75,11 @@ if __name__ == "__main__":
         
         results_plot = obj_det.detect_objects(frame, filter_class=True)
         bb_center = obj_det.boundingboxcenter(results_plot)
+
+        if bb_center != None:
+            for i in range(len(bb_center)):
+                cv2.circle(results_plot, bb_center[i,:].tolist(), 2, (0, 255, 0), -1)
+
         cv2.imshow("YOLOv8", results_plot)
         
         if cv2.waitKey(10) & 0xFF == ord('q'): #press q to quit
